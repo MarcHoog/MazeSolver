a2de155901ad2e04e8294f677106f1d21e65636d
Marc Hoogendoorn <Marc.Hoogendoorn@simac.com>
2024-08-05 22:28:26 +0200

    ✨ Added new lines and graphics

--- src/graphics.py
+++ src/graphics.py
@@ -1,6 +1,60 @@
 from tkinter import Tk, BOTH, Canvas
 
-class Window():
+class Point:
+    
+    def __init__(self, x, y):
+        self.x = x # x coordinate Horizontal
+        self.y = y # y coordinate Vertical
+
+
+class Line:
+    
+    def __init__(self, start:Point, end: Point):
+        self.start = start
+        self.end = end
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(self.start.x, self.start.y, 
+                           self.end.x, self.end.y, 
+                           fill=fill_color)
+
+
+class Cell:
+    
+    def __init__(self,window, width, height, x, y):
+        self.window = window        
+
+        self.width = width
+        self.height = height
+        
+        self.x = x
+        self.y = y
+        
+        self.x2 = x + width
+        self.y2 = y + height
+        
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        
+    def draw(self, fill_color):
+        if self.has_left_wall:
+            self.window.draw_line(Line(Point(self.x, self.y), Point(self.x, self.y2)), fill_color)
+        if self.has_right_wall:
+            self.window.draw_line(Line(Point(self.x2, self.y), Point(self.x2, self.y2)), fill_color)
+        if self.has_top_wall:
+            self.window.draw_line(Line(Point(self.x, self.y), Point(self.x2, self.y)), fill_color)
+        if self.has_bottom_wall:
+            self.window.draw_line(Line(Point(self.x, self.y2), Point(self.x2, self.y2)), fill_color)
+        
+    def __str__(self):
+        return f'Cell(x={self.x}, y={self.y}, width={self.width}, height={self.height})'
+    
+    def __repr__(self):
+        return str(self)
+
+class Window:
     
     def __init__(self, width, height):
         self.width = width
@@ -28,4 +82,7 @@ class Window():
     def close(self):
         self.__running = False
         return self.__running
+    
+    def draw_line(self, line:Line, fill_color):
+        line.draw(self.__canvas, fill_color)
     
--- src/main.py
+++ src/main.py
@@ -1,7 +1,13 @@
-from graphics import Window
+from graphics import Window, Line, Point
 
 def main():
     window = Window(800, 600)
+    line1 = Line(Point(100, 100), Point(200, 200))
+    line2 = Line(Point(200, 200), Point(300, 100))
+    line3 = Line(Point(300, 100), Point(400, 200))
+    window.draw_line(line1, 'red')
+    window.draw_line(line2, 'green')
+    window.draw_line(line3, 'blue')
     window.wait_to_close()
     
 if __name__ == '__main__':
