a257faad82d5125a36116f5f3afbcefc7d5e52d3
Marc Hoogendoorn <Marc.Hoogendoorn@simac.com>
2024-08-05 22:51:33 +0200

    fixed draw class

--- src/cell.py
+++ src/cell.py
@@ -3,34 +3,46 @@ from graphics import Window, Line, Point
 class Cell:
     
     def __init__(self,window, width, height, x, y):
-        self.window = window        
+        self._window = window        
 
-        self.width = width
-        self.height = height
+        self._width = width
+        self._height = height
         
-        self.x = x
-        self.y = y
+        self._x = x
+        self._y = y
         
-        self.x2 = x + width
-        self.y2 = y + height
+        self._x2 = x + width
+        self._y2 = y + height
         
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
         
-    def draw(self, fill_color):
+    def draw(self, fill_color='black'):
         if self.has_left_wall:
-            self.window.draw_line(Line(Point(self.x, self.y), Point(self.x, self.y2)), fill_color)
+            self._window.draw_line(Line(Point(self._x, self._y), Point(self._x, self._y2)), fill_color)
         if self.has_right_wall:
-            self.window.draw_line(Line(Point(self.x2, self.y), Point(self.x2, self.y2)), fill_color)
+            self._window.draw_line(Line(Point(self._x2, self._y), Point(self._x2, self._y2)), fill_color)
         if self.has_top_wall:
-            self.window.draw_line(Line(Point(self.x, self.y), Point(self.x2, self.y)), fill_color)
+            self._window.draw_line(Line(Point(self._x, self._y), Point(self._x2, self._y)), fill_color)
         if self.has_bottom_wall:
-            self.window.draw_line(Line(Point(self.x, self.y2), Point(self.x2, self.y2)), fill_color)
+            self._window.draw_line(Line(Point(self._x, self._y2), Point(self._x2, self._y2)), fill_color)
+    
+    def __get_move_color(self, undo):
+        if undo:
+            return 'red'
+        else:
+            return 'gray'
+    
+    def draw_move(self, to_cell, undo=False):
+        center =  Point(self._x + self._width // 2, self._y + self._height // 2)
+        to_center = Point(to_cell._x + to_cell._width // 2, to_cell._y + to_cell._height // 2)    
+        
+        self._window.draw_line(Line(center, to_center), self.__get_move_color(undo))
         
     def __str__(self):
-        return f'Cell(x={self.x}, y={self.y}, width={self.width}, height={self.height})'
+        return f'Cell(x={self._x}, y={self._y}, width={self._width}, height={self._height})'
     
     def __repr__(self):
         return str(self)
--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from graphics import Window,
+from graphics import Window
 from cell import Cell 
 
 def main():
@@ -7,9 +7,10 @@ def main():
     cell2 = cell = Cell(window, 100, 100, 300, 200)
     cell3 = cell = Cell(window, 100, 100, 400, 200)
     cell1.has_left_wall = False
-    cell1.draw('red')
-    cell2.draw('green')
-    cell3.draw('blue')
+    cell1.draw()
+    cell2.draw()
+    cell3.draw()
+    cell1.draw_move(cell2, undo=True)
     window.wait_to_close()
     
 if __name__ == '__main__':
